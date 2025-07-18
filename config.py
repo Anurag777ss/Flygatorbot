eb3e38ec3b9e7550bc113421976937d3b26943b1
Anurag777ss <tripbuddycare@gmail.com>
2025-07-19 02:57:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TRAVELPAYOUT_API_KEY = os.getenv("TRAVELPAYOUT_API_KEY")
-
-if not TELEGRAM_TOKEN or not TRAVELPAYOUT_API_KEY:
-    raise Exception("⚠️ TELEGRAM_TOKEN or TRAVELPAYOUT_API_KEY is missing in environment variables!")
